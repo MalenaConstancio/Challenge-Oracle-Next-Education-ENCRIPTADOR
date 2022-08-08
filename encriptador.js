01c235db174019483441b5c0602af51afaf7daa6
MaleConstancio <106634099+MaleConstancio@users.noreply.github.com>
2022-08-08 16:16:06 -0300

    creando archivos challenge Encriptador Oracle Next Education

---
+++ encriptador.js
@@ -0,0 +1,45 @@
+
+const encriptado = {a:"ai",e:"enter",i:"imes",o:"ober",u:"ufat"};
+document.getElementById("textoEncriptado").style.display = "none";
+
+function encriptar(){
+
+    document.getElementById("mensajeNoEncontrado").style.display = "none";
+    document.getElementById("textoEncriptado").style.display = "inline";
+    let texto =  document.getElementById("texto").value;
+    let textoEncriptado = texto.replace(/[aeiou]/g,i=>encriptado[i]);
+    
+    return textoEncriptado;
+}
+
+function validarMayusculas() {    
+    
+        if(document.getElementById("texto").value.match(/^[a-z ]*$/)) {
+            document.getElementById("tres").value = encriptar();
+        } else {
+            alert("Solo letras minúsculas, sin acentos.");
+        }
+}
+
+function desencriptar(){
+    let textoEncriptado = document.getElementById("tres").value;
+    let texto= textoEncriptado.replace(/ai/g,"a")
+    .replace(/enter/g,"e")
+    .replace(/imes/g,"i")
+    .replace(/ober/g,"o")
+    .replace(/ufat/g,"u");
+    document.getElementById("tres").value= texto;
+}
+
+function copiar(){
+    let textoCopiado = document.getElementById("tres");
+    textoCopiado.select();
+    document.execCommand('copy');
+    alert("Se copio el texto en el portapapeles");
+}
+
+
+
+    
+
+
